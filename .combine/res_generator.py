21ad91f235311afb4baa348c2d2d313402c2bb50
Jacksgong <igzhenjie@gmail.com>
2017-07-03 22:32:11 +0800

    chore: declare apache2 license for whole project

--- .combine/res_generator.py
+++ .combine/res_generator.py
@@ -1,11 +1,26 @@
+#!/usr/bin/python -u
+
+"""
+Copyright (C) 2017 Jacksgong(blog.dreamtobe.cn)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 # This python script used for generating mock resources for multiple modules on multiple projects.
-#
-# @author jacks.gong
-# @date: 2017.04.19
 
 import re
-from os import listdir, makedirs, walk
-from os.path import abspath, exists, isdir, isfile, join, pardir
+from os import makedirs, walk
+from os.path import exists, isfile, join
 from shutil import copyfile
 from xml.etree.ElementTree import Element, SubElement, tostring
 
